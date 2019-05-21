4998b7eeef3709a23cef151f571940bf5a9e39cd
Gary Allin <gary.allin@deputi.ai>
2019-05-21 09:16:04 +0100

    Forecast

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -4,7 +4,7 @@ const Forecast = props => {
   console.log(props);
   return (
     <React.Fragment>
-      <h3>Forecast for next few days</h3>
+      <h3>Forecast for next 3 days</h3>
     </React.Fragment>
   );
 };
